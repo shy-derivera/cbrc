4dca3448e30971c83875ec5d9c8b0ff97efae4c3
kencreations <138158098+kencreations@users.noreply.github.com>
2023-08-02 17:41:47 +0800

    Update edit_student.php
    fixed error update student

--- admin/edit_student.php
+++ admin/edit_student.php
@@ -1,8 +1,8 @@
 <?php
 include ("connection.php");
-$g = $_GET["id"];
-$sql = "UPDATE es_students SET `status`='VERIFIED' WHERE student_numer='$g'";
+$g = $_GET["stud_num"];
+$sql = "UPDATE es_students SET `status`='VERIFIED' WHERE student_number='$g'";
 mysqli_query($conn, $sql);
 
-echo "<script>window.location.href='dashboard.php'</script>";
+echo "<script>window.location.href='student_list.php'</script>";
 ?>
