69f9ad147c3800051d0bcc626704243e44e764f7
shy-derivera <54820127+shy-derivera@users.noreply.github.com>
2023-08-02 15:19:12 +0800

    Create logo.php

---
+++ includes/logo.php
@@ -0,0 +1,7 @@
+<div class="sidebar-header">
+				<div>
+					<img src="../includes/images/logo.png" width="150"  alt="logo icon">
+				</div>
+				<div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+				</div>
+</div>
